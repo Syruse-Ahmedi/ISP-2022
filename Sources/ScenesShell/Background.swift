f64aa518067be2632623ba237a56d95a22118730
Kevin Li <kevin@li.org>
2022-04-21 16:04:28 -0500

    background

--- Sources/ScenesShell/Background.swift
+++ Sources/ScenesShell/Background.swift
@@ -7,9 +7,16 @@ import Igis
 
 
 class Background : RenderableEntity {
-
+    let MaxScenes = 50
+    var CurrentScene = 0
+    
       init() {
           // Using a meaningful name can be helpful for debugging
           super.init(name:"Background")
       }
+      func renderArrows(to canvas: Canvas, direction:String){
+          if direction == Left{
+          }
+      }
+      
 }
